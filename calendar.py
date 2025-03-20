59a9b7349498a0fe7450fd3f6cdd15f32fcc9c4b
wlgkcjf06 <json019@snu.ac.kr>
2025-03-20 17:22:13 +0900

    HW1 - Display Calendar Month (fixed final ver)

--- calendar.py
+++ calendar.py
@@ -13,26 +13,36 @@ def get_year():
     return y
 
 #윤년인지 판단
-def ifleap(year):
-    if year%400==0:
+def ifleap(y):
+    if y%400==0:
         return True
-    elif year%100==0:
+    elif y%100==0:
         return False
-    elif year%4==0:
+    elif y%4==0:
         return True
     else:
         return False
 
+def getdays(m, l):
+    if m in [1,3,5,7,8,10,12]:
+        d = 31
+    elif month in [4,6,9,11]:
+        d = 30
+    elif month ==2 and l:
+        d = 29
+    else:
+        d = 28
+    return d
 
 #입력받은 달이 시작하는 요일 구하기
-def zeller(month,year):
-    if month <= 2:
-        month = month + 12
-        year = year - 1
-    y = year % 100
-    c = year // 100
-    sday = (1 + ((month + 1) * 13)//5 + y + y//4 + c//4 - 2 * c) % 7
-    return sday
+def zeller(m,ye):
+    if m <= 2:
+        m += 12
+        ye -= 1
+    y = ye % 100
+    c = ye // 100
+    res = (1 + ((m + 1) * 13)//5 + y + y//4 + c//4 - 2 * c) % 7
+    return res
 
 #토요일 0 -> 일요일 0으로 바꾸기
 def shiftdays(d):
@@ -42,14 +52,28 @@ def shiftdays(d):
         d=d-1
     return d
 
-'''def removeemptyrow(arr):
-    for i in range(6):
-        check = False
-        for j in range(7):
-            if arr[i][j]!=0:
-                check = True
-        if check:'''
+def getrows(s, d):
+    k = (s+d)//7
+    if s+d%7!=0:
+        k+=1
+    return k
 
+def makearray(s, d, r):
+    tmp=[]
+    c = 1
+    for i in range(r):
+        tmp.append([])
+        for j in range(7):
+            tmp[i].append(0)
+            if i == 0:
+                if j>=s:
+                    tmp[i][j]=c
+                    c+=1
+            else:
+                if c<=d:
+                    tmp[i][j]=c
+                    c+=1
+    return tmp
 
 def displaymonthyear(m,y):
     mlist=['January','Febuary','March','April','May','June','July','August','September','October','November','December']
@@ -58,59 +82,34 @@ def displaymonthyear(m,y):
     print(out)
 
 #달력 표시
-def displaycalendar(arr,r):
+def displaycalendar(mon,r):
     print('Su Mo Tu We Th Fr Sa')
     c=1
     for i in range(r):
         for j in range(7):
-            if arr[i][j]==0:
+            if mon[i][j]==0:
                 print('   ',end='')
             else:
                 if c<=9:
-                    print(' '+str(arr[i][j])+' ',end="")
+                    print(' '+str(mon[i][j])+' ',end="")
                     c=c+1
                 else:
-                    print(str(arr[i][j])+' ',end='')
+                    print(str(mon[i][j])+' ',end='')
         print('')
 
 
+print('This Program will display a calendar month between 1800 and 2099')
 while 1:
-    print('This Program will display a calendar month between 1800 and 2099')
     month = get_month()
     if month == -1:
         break
     year = get_year()
     leap = ifleap(year)
-    if month in [1,3,5,7,8,10,12]:
-        days = 31
-    elif month in [4,6,9,11]:
-        days = 30
-    elif month ==2 and leap:
-        days = 29
-    else:
-        days = 28
+    days = getdays(month, leap)
     startday = zeller(month, year)
     startday = shiftdays(startday)
-    if startday+days==28:
-        rows = 4
-    elif startday+days<=35:
-        rows = 5
-    else:
-        rows = 6
-    arr=[]
-    counter = 1
-    for i in range(rows):
-        arr.append([])
-        for j in range(7):
-            arr[i].append(0)
-            if i == 0:
-                if j>=startday:
-                    arr[i][j]=counter
-                    counter=counter+1
-            else:
-                if counter<=days:
-                    arr[i][j]=counter
-                    counter=counter+1
+    rows = getrows(startday, days)
+    arr = makearray(startday, days, rows)
     displaymonthyear(month,year)
     displaycalendar(arr,rows)
 
